2f3d962ba140560a9142602322e2dcaeb6449f23
A-Awad1 <eng.a.awad1@gmail.com>
2023-07-12 17:47:18 +0300

    updates in calc logic

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,65 @@
+let keypad = document.querySelector(".keypad"),
+  valuesKeys = document.querySelectorAll("button[data-value]"),
+  operatorsKeys = document.querySelectorAll("button[data-operator]"),
+  delButton = document.querySelector(".delete"),
+  resetButton = document.querySelector(".reset"),
+  equalButton = document.querySelector(".equal"),
+  payload = "",
+  operator = "",
+  digits = [],
+  result = 0;
+
+keypad.textContent = 0;
+
+function displayResult() {
+  if (digits.length === 1 && operator !== "" && payload !== "") {
+    digits[1] = payload;
+    result = eval(`${+digits[0]} ${operator} ${+digits[1]}`);
+    keypad.textContent = result;
+    digits = [];
+    payload = result;
+    operator = "";
+  } else {
+  }
+}
+valuesKeys.forEach(
+  (e) =>
+    (e.onclick = () => {
+      payload += e.dataset.value;
+      payload = +payload;
+      keypad.textContent = payload;
+    })
+);
+operatorsKeys.forEach(
+  (e) =>
+    (e.onclick = () => {
+      displayResult();
+      if (payload !== "") {
+        if (digits.length === 0) {
+          digits[0] = payload;
+        }
+        operator = e.dataset.operator;
+        payload = "";
+      }
+    })
+);
+equalButton.onclick = () => {
+  displayResult();
+};
+delButton.onclick = () => {
+  console.log(payload);
+  payload = payload.toString().slice(0, -1);
+  payload === "" ? (payload = 0) : null;
+  payload = +payload;
+  keypad.textContent = payload;
+};
+resetButton.onclick = () => {
+  payload = "";
+  operator = "";
+  digits = [];
+  result = 0;
+  keypad.textContent = 0;
+};
 
 let theme = localStorage.theme || 1;
 document.documentElement.dataset.theme = theme;
